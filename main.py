2bb200eaefb795fa10e90ef9efdd02811ad31201
omer <omersenol30@gmail.com>
2023-11-20 12:38:27 +0300

    fixed FileNotFoundError

--- main.py
+++ main.py
@@ -45,15 +45,20 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="ERROR", message="Please fill the empty spaces")
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             data.update(new_data)
-        with open("data.json", "w") as data_file:
+            with open("data.json", "w") as data_file:
 
-            json.dump(data, data_file, indent=4)
-
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+                json.dump(data, data_file, indent=4)
+        finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
